4055bdad06bdd3f70cb5a0bb222158490bfa4954
nancy9719 <134652851+nancy9719@users.noreply.github.com>
2023-11-21 14:09:27 -0600

    Deleted model/controller/view folders. Moved any still potentially useful code from the folders to PetProfile.php and Registration.php. Deleted any empty files

--- design/Registration.php
+++ design/Registration.php
@@ -76,3 +76,107 @@
     </div>
 </body>
 </html>
+
+
+
+//code has been moved from signup.php and customerController.php files in case 
+//it provides any use still. Remove if unnecessary
+<?php
+
+    private function emptyInput()
+    {
+        $result;
+        if (empty($this->$name) ||
+            empty($this->$email) ||
+            empty($this->$phone) ||
+            empty($this->$address) ||
+            empty($this->$password) ||
+            empty($this->$password_confirmation)) 
+        {
+            $result = false; 
+        } 
+        else 
+        {
+            $result = true;
+        }
+        return $result;        
+    }
+
+    private function invalidCharacters($input)
+    {
+        $result;
+        if (!preg_match("/^[a-zA-Z0-9]*$/", $input)) {
+            $result = false;
+        }
+        else 
+        {
+            $result = true;
+        }
+        return $result;
+    }
+    
+    private function invalidEmail()
+    {
+        $result;
+        if (!filter_var($this->$email, FILTER_VALIDATE_EMAIL))
+        {
+            $result = false;
+        }
+        else 
+        {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function pwdMatch() 
+    {
+        $result;
+        if ($this->$password !== $this->$password_confirmation)
+        {
+            $result = false;
+        }
+        else 
+        {
+            $result = true;
+        }
+        return $result;
+    }
+
+}
+
+if(isset($_POST["submit "])) {
+
+    // Grabbing the data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $address = $_POST["address"];
+    $password = $_POST["password"];
+    $password_confirmation = $_POST["password_confirmation"];
+    
+    // Instantiate CustomerController class
+    include "../constrollers/CustomerController.php";
+    $new_customer = new CustomerController($name, $email, $phone, $address, $password, $password_confirmation);
+
+
+    // Running error handlers and signup
+
+
+    // Going back to front page
+
+}
+
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+$mysqli = require __DIR__."../db_connection.php";
+
+// $sql = "INSERT INTO user (name, email, password_hash)
+//         VALUES (?, ?, ?)";
+
+// $stmt = $mysqli->stmt_init();
+
+print_r($_POST);
+// var_dump($password_hash);
+
+?> 
