18683e158f581bd9322c4a113bbaeeee0941507a
rudyruiz01 <r_ruiz.3486@yahoo.com>
2023-11-25 17:24:11 -0600

    Added Customer model

--- models/Customer.php
+++ models/Customer.php
@@ -10,8 +10,8 @@ class Customer {
     private $password;
     private $password_confirmation;
   
-    public function __construct($first, $last, $address, $phone, $email, $password, $password_confirmation) {
-
+    public function __construct($first, $last, $address, $phone, $email, $password, $password_confirmation) 
+    {
         $this->first = $first;
         $this->last = $last;
         $this->address = $address;
@@ -19,41 +19,63 @@ class Customer {
         $this->email = $email;        
         $this->password = $password;
         $this->password_confirmation = $password_confirmation;
-        
     }
 
     // Setter Methods
-    function set_first($first)
+    function setFirst($first)
     {
         $this->first = $first;
     }
 
-    function set_last($last)
+    function setLast($last)
     {
         $this->last = $last;
     }
 
-    function set_address($address)
+    function setAddress($address)
     {
         $this->address = $address;
     }
     
-    function set_phone($phone)
+    function setPhone($phone)
     {
         $this->phone = $phone;
     }    
 
-    function set_email($email)
+    function setEmail($email)
     {
         $this->email = $email;
     }    
 
-    function set_password($password)
+    function setPassword($password)
     {
         $this->password = $password;
     }
 
     // Getter Methods 
+    function getFirst()
+    {
+        return $this->first;
+    }
 
+    function getLast()
+    {
+        return $this->last;
+    }
 
-    
+    function getAddress()
+    {
+        return $this->address;
+    }
+
+    function getPhone()
+    {
+        $this->phone;
+    }
+
+    function getEmail()
+    {
+        return $this->email;
+    }    
+
+}
