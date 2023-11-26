d119954d8ae5bc4f3457d2ec1360d891a9f66bf1
Tiana <tiana.aakins95@gmail.com>
2023-11-26 13:50:15 -0600

    completed design for home page

---
+++ design/Home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="container">
+        <div class="main">
+            <img src="PawSalonIcon.png" class="rounded-circle" width="150">
+            <img src="PawSalon.png" class="rounded-circle" width="350">
+            <h1>Home</h1>
+            <div class="topbar">
+                <h2>Customer Support: 254-323-3421</h2>
+                <a href="Contact.php">Contact Us</a>
+                <a href="Gallery.php">Gallery</a>
+                <a href="Home.php">Home</a>
+            </div>
+            <hr>
+        </div>
+        <center>
+            <div class="wrapper3">
+                <h3>Welcome to The Paw Salon</h3>
+                <h4> The Paw Salon is a friendly grooming business ready to take care of your special family members.
+                    We offer many services including baths, haircuts, and even packaged deals! </h4>
+                <h4> Follow the links below to access your personal account to create pet profiles, view services, and book appointmets. </h4>
+                <h3> <i style="font-size:24px" class="fa">&#xf1b0;</i>     Come Join the Family!    <i style="font-size:24px" class="fa">&#xf1b0;</i></h3>
+
+            </div>
+        </center>
+
+        <center>   
+        <img src="home1.jpg" width="350" height="350" style="border-radius: 25px; ">
+        <img src="home2.jpg" width="350" height="350" style="border-radius: 25px;">  
+        <img src="home3.jpg" width="350" height="350" style="border-radius: 25px;">  
+        <img src="home4.jpg" width="350" height="350" style="border-radius: 25px;">   
+        <hr>
+        </center>
+
+        <center>
+        <div class="wrapper2">
+            <h3> Click Below To Register</h3>
+            <button onclick="location.href='Registration.php'" type="submit" class="btn" style="margin-left:27px; width: 300px; height: 60px;">Register</button>
+            <br/> 
+            <h3> Click Below To Login</h3>
+            <button onclick="location.href='Login.php'" type="submit" class="btn" style="margin-left:27px; width: 300px; height: 60px;">Login</button>
+        </div>
+        </center>
+
+    </div>
+</body>
+</html>
