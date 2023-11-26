3cd5270ec9ccba705812d56ed380bb30594140c5
Tiana <tiana.aakins95@gmail.com>
2023-11-26 13:46:03 -0600

    completed design changes for gallery page. still needs images

---
+++ design/Gallery.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Gallery.css">
+    <title>Gallery</title>
+</head>
+<body>
+    <div class="container">
+        <div class="main">
+            <img src="PawSalonIcon.png" class="rounded-circle" width="150">
+            <img src="PawSalon.png" class="rounded-circle" width="350">
+            <h1>Gallery</h1>
+            <div class="topbar">
+                <h2>Customer Support: 254-323-3421</h2>
+                <a href="Contact.php">Contact Us</a>
+                <a href="Gallery.php">Gallery</a>
+                <a href="Home.php">Home</a>
+            </div>
+            <hr>
+        </div>
+    </div>
+</body>
+</html>
