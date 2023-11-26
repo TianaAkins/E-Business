70ebe7769e6cfd49fafaa23f9845f8f49494815b
Tiana <tiana.aakins95@gmail.com>
2023-11-26 14:00:47 -0600

    completed design for services change

--- design/Services.php
+++ design/Services.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Services.css">
-    <title>Document</title>
+    <title>Services</title>
 </head>
 <body>
     <div class="container">
@@ -14,13 +14,62 @@
             <h1>Services</h1>
             <div class="topbar">
                 <h2>Customer Support: 254-323-3421</h2>
-                <a href="Login.html">Logout</a>
-                <a href="Appointments.html">Appointments</a>
-                <a href="Services.html">Services</a>
-                <a href="PetProfile.html">Pet Profile</a>
-                <a href="CustomerProfile.html">Customer Profile</a>
+                <a href="Home.php">Logout</a>
+                <a href="Contact.php">Contact Us</a>
+                <a href="Gallery.php">Gallery</a>
+                <a href="Appointments.php">Appointments</a>
+                <a href="Services.php">Services</a>
+                <a href="PetProfile.php">Pet Profile</a>
+                <a href="CustomerProfile.php">Customer Profile</a>
             </div>
             <hr>
+            <center>
+                <div class="wrapper">
+                    <h3>Nail Trim</h3>
+                    <h3>$25.00</h3>
+                    <h4>Includes nail trim, file, and buffer.</h4>
+                    
+                    <hr>
+                    <img src="NailTrim.jpg" width="250">
+    
+                </div>
+    
+                <div class="wrapper">
+                    <h3>Haircut</h3>
+                    <h3>$50.00</h3>
+                    <h4>Includes cut and style.</h4>
+                    <hr>
+                    <img src="Haircut.jpg" width="250">
+        
+                </div>
+                <br/>
+                <br/>
+                <br/>
+    
+                <div class="wrapper">
+                    <h3>Bath & Brush</h3>
+                    <h3>$75.00</h3>
+                    <h4>Includes deep shampoo and conditioner, blow dry and de-shed.</h4>
+                    <hr>
+                    <img src="Bath.jpg" width="250">
+        
+                </div>
+    
+                <div class="wrapper">
+                    <h3>Full Service Package Deal!</h3>
+                    <h3>$90.00</h3>
+                    <h4>Includes nail services, bath, de-shed, haircut, and teeth cleaning.</h4>
+                    <hr>
+                    <img src="Teeth.jpg" width="250">
+        
+                </div>
+            </center>
+            
+            
+
+
+            
+            
         </div>
     </div>
 </body>
