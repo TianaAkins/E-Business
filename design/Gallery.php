ec1c3c50556bab9e44f2b809223997b45f2ea856
Tiana <tiana.aakins95@gmail.com>
2023-12-10 02:03:37 -0600

    completed design changes to gallery page.

--- design/Gallery.php
+++ design/Gallery.php
@@ -9,17 +9,50 @@
 <body>
     <div class="container">
         <div class="main">
-            <img src="PawSalonIcon.png" class="rounded-circle" width="150">
-            <img src="PawSalon.png" class="rounded-circle" width="350">
+            <a href="Home.php"><img src="images/PawSalonIcon.png" class="rounded-circle" width="150">
+            <img src="images/PawSalon.png" class="rounded-circle" width="350"></a>
             <h1>Gallery</h1>
             <div class="topbar">
                 <h2>Customer Support: 254-323-3421</h2>
+                <a href="Home.php">Logout</a>
                 <a href="Contact.php">Contact Us</a>
                 <a href="Gallery.php">Gallery</a>
+                <a href="Appointments.php">Appointments</a>
+                <a href="Services.php">Services</a>
+                <a href="PetProfile.php">Pet Profile</a>
+                <a href="CustomerProfile.php">Customer Profile</a>
                 <a href="Home.php">Home</a>
             </div>
             <hr>
         </div>
     </div>
+ 
+    <div class="gallery">
+        <a href="Services.php">
+            <img src="images/grooming1.jpg" alt="Cinque Terre" width="600" height="400">
+        </a>
+        <div class="desc">Quality care from our grooming specialist</div>
+    </div>
+    
+   
+      
+      <div class="gallery">
+        <a href="Services.php">
+          <img src="images/grooming2.jpg" alt="Forest" width="600" height="400">
+        </a>
+        <div class="desc">Guaranteed smiles from our fury friends!</div>
+      </div>
+      
+      <div class="gallery">
+        <a href="Services.php">
+          <img src="images/grooming3.jpg" alt="Northern Lights" width="600" height="400">
+        </a>
+        <div class="desc">Even your grumpy pets will love our services! </div>
+      </div>
+      
+      <div class="wrapper">
+        
+        <div class="desc"><a href="Services.php"> Click Here To Book Your Next Service</a></div>
+      </div>
 </body>
 </html>
